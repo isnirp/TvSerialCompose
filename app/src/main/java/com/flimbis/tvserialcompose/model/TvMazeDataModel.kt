f9969af5bd2213da30b6c40c7afde4f21cadd3e1
isnirp <pramankwah@gmail.com>
2023-02-23 22:19:18 +0100

    define tv maze model

---
+++ app/src/main/java/com/flimbis/tvserialcompose/model/TvMazeDataModel.kt
@@ -0,0 +1,14 @@
+package com.flimbis.tvserialcompose.model
+
+data class Image(val medium: String, val original: String)
+
+data class Episodes(val id: Long, val name: String, val season: Int, val number: Int,
+                    val runtime: Int, val image: Image, val summary: String?)
+
+data class Seasons(val id: Long, val number: Int, val name: String?, val episodeOrder: Int?, val premiereDate: String?, val endDate: String?,
+                   val image: Image, val summary: String?)
+
+data class Schedule(val time: String, val days: List<String>)
+
+data class Shows(val id: Long, val url: String, val name: String, val genres: List<String>, val status: String,
+                 val schedule: Schedule, val image: Image, val summary: String?)
