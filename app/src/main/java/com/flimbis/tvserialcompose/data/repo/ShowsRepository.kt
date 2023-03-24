42aac67f4d06db04f7293736da1b0c9df304843b
isnirp <pramankwah@gmail.com>
2023-03-24 12:12:13 +0100

    define shows repository

---
+++ app/src/main/java/com/flimbis/tvserialcompose/data/repo/ShowsRepository.kt
@@ -0,0 +1,28 @@
+package com.flimbis.tvserialcompose.data.repo
+
+import com.flimbis.tvserialcompose.data.ShowsDataSource
+import com.flimbis.tvserialcompose.model.Shows
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+interface ShowsRepository {
+    suspend fun getAll(pageNumber: Int): List<Shows>
+
+    suspend fun get(id: Long): Shows?
+}
+
+class ShowsRepositoryImpl(private val dataSource: ShowsDataSource) : ShowsRepository {
+    override suspend fun getAll(pageNumber: Int): List<Shows> {
+        return withContext(Dispatchers.IO) /*switch context; makes coroutine cancelable by default*/{
+            dataSource.getAll(pageNumber)
+        }
+    }
+
+    override suspend fun get(id: Long): Shows? {
+       return withContext(Dispatchers.IO) {
+           dataSource.get(id)
+       }
+    }
+
+}
