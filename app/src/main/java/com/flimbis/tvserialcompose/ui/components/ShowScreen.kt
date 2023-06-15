94a0df7f9483be2bda342a2c1543604a835d04de
isnirp <pramankwah@gmail.com>
2023-06-15 23:07:57 +0200

    show details screen design

--- app/src/main/java/com/flimbis/tvserialcompose/ui/components/ShowScreen.kt
+++ app/src/main/java/com/flimbis/tvserialcompose/ui/components/ShowScreen.kt
@@ -1,11 +1,17 @@
 package com.flimbis.tvserialcompose.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun ShowScreen(
@@ -17,7 +23,20 @@ fun ShowScreen(
         text = "Hello Navigation $id",
         modifier = Modifier.padding(paddingValues.calculateTopPadding())
     )
-    LaunchedEffect(true) {
-        onShowDetails
+    Box(modifier = Modifier.fillMaxSize()) {
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+        Column(modifier = Modifier.padding(32.dp)) {
+            Text(
+                text = "title",
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(text = "description")
+
+        }
     }
 }
