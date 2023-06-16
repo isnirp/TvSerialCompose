23ee076ae67fa47b3fa2e3daa4e196506191e54d
isnirp <pramankwah@gmail.com>
2023-06-16 17:10:27 +0200

    shows detail view model

--- app/src/main/java/com/flimbis/tvserialcompose/ui/vm/ShowDetailsViewModel.kt
+++ app/src/main/java/com/flimbis/tvserialcompose/ui/vm/ShowDetailsViewModel.kt
@@ -1,4 +1,34 @@
 package com.flimbis.tvserialcompose.ui.vm
 
-class ShowDetailsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.flimbis.tvserialcompose.data.repo.ShowsRepository
+import com.flimbis.tvserialcompose.model.Shows
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ShowDetailsViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    val showsRepository: ShowsRepository
+) : ViewModel() {
+
+    private val showId: Long = checkNotNull(savedStateHandle["id"])
+
+    private val _show = MutableLiveData<Shows>()
+    val show: LiveData<Shows> = _show
+
+    init {
+        getShow(showId)
+    }
+
+    fun getShow(id: Long) {
+        viewModelScope.launch {
+            _show.value = showsRepository.get(id)
+        }
+    }
 }
